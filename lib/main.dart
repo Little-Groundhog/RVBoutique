3bfa62b4987987427e5ad4d088e0042cf4cc056d
Maxime Belcour <maximebelcour@gmail.com>
2020-03-26 15:24:57 +0100

    [REFAC] ajout de commentaire pour demo

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:ChaMaxAdr/starting_page.dart';
-
+/*
+* test
+* test
+* test
+*
+* */
 main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
-  @override
+  @override//test
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,

7458d946e271b77320069bfd76f00fcb08907689
Maxime Belcour <maximebelcour@gmail.com>
2020-04-26 19:36:21 +0200

    [FEATURE] Ajout des codes pour accéder au modèles 3D

---
+++ lib/code_boutique.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+
+import 'package:ChaMaxAdr/model_event.dart';
+import 'package:ChaMaxAdr/starting_page.dart' as strings;
+
+class TextEntree {
+  //String code = starting_page:Text(TextEntree.text);
+
+}
--- lib/strings.dart
+++ lib/strings.dart
@@ -1,10 +1,21 @@
 String start = "GO !";
+
+//Liste des modèles 3D disponibles
 String statuePrefab = "statueModel";
 String dronePrefab = "droneModel";
 String cubePrefab = "cubeModel";
 String brainPrefab = "brainModel";
 String catPrefab = "catModel";
 String legoHousePrefab = "legoHouseModel";
+
+//Liste des codes pour obtenir
+String statue = "0000";
+String drone = "1111";
+String cube = "2222";
+String brain = "3333";
+String cat = "4444";
+String lego = "5555";
+
 String aboutApp = "Alors elle est pas génial notre application ? \n"
     "Pour retrouver le code source de l'application, il suffit de cliquer sur "
     "le petit bouton Git juste en dessous ;) \n"

f750bdbfce6ac0228ad138467c42b7e87c24b819
Maxime Belcour <maximebelcour@gmail.com>
2020-05-01 13:50:29 +0200

    [FEATURES] Changement de page d'acceuil

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:ChaMaxAdr/starting_page.dart';
-/*
-* test
-* test
-* test
-*
-* */
+import 'package:ChaMaxAdr/New_starting_page.dart';
+
 main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
-  @override//test
+  @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
 
     return MaterialApp(
-      home: StartingPage(),
+      home: MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
